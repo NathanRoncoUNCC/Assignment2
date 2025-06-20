e4bf51c50abf408509187d8f37f4216f3d4ecdea
NathanRoncoUNCC <nronco@charlotte.edu>
2025-06-20 09:38:51 -0400

    Imported functions for cashier.py

--- cashier.py
+++ cashier.py
@@ -3,11 +3,21 @@ class Cashier:
         pass
 
     def process_coins(self):
-        """Returns the total calculated from coins inserted.
-           Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        quarters = int(input("How many quarters?: "))  # 0.25
+        dimes = int(input("How many dimes?: "))  # 0.10
+        nickels = int(input("How many nickels?: "))  # 0.05
+        pennies = int(input("How many pennies?: "))  # 0.01
+        total = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+        return round(total, 2)
 
     def transaction_result(self, coins, cost):
-        """Return True when the payment is accepted, or False if money is insufficient.
-           Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            if change > 0:
+                print(f"Change: ${change}")
+            print("Payment accepted.")
+            return True
+        else:
+            print("Not enough funds.")
+            return False
