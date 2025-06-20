9f9d5a817882400fd2f2dc8ba649f8ac6fb4c507
NathanRoncoUNCC <nronco@charlotte.edu>
2025-06-20 09:39:19 -0400

    Imported functions for sandwich_maker.py

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -3,8 +3,13 @@ class SandwichMaker:
         self.machine_resources = resources
 
     def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources.get(item, 0):
+                print(f"Sorry, not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"Enjoy your {sandwich_size} ham sandwich.")
